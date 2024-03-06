d6ef23289ac5a6003ed47e2f8df16392c052cfe9
ryanpanta <ryan.rodrigues62@hotmail.com>
2024-03-06 11:45:14 -0300

    first commit

---
+++ quizz/src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Quiz from "../img/quiz.svg";
+
+import "./Welcome.css";
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img src={Quiz} alt="Início do Quizz" />
+    </div>
+  );
+};
+
+export default Welcome;
