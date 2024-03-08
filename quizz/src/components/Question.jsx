63c56921d50e8796b96df63579b747bd6797fb2f
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-03-07 22:02:39 -0300

    versao fisioterapia

---
+++ quizz/src/components/Question.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+import Option from "./Option";
+import "./Question.css";
+
+const Question = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
+
+    const onSelectOption = (option) => {
+        dispatch({
+            type: "CHECK_ANSWER",
+            payload: {answer: currentQuestion.answer, option}
+        })
+    };
+    return (
+        <div id="question">
+            <p>
+                Pergunta {quizState.currentQuestion + 1} de{" "}
+                {quizState.questions.length}
+            </p>
+            <h2>{currentQuestion.question}</h2>
+            <div id="options-container">
+                {currentQuestion.options.map((option) => (
+                    <Option
+                        key={option}
+                        option={option}
+                        answer={currentQuestion.answer}
+                        selectOption={() => onSelectOption(option)}
+                    />
+                ))}
+            </div>
+            {quizState.answerSelected && (
+                <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
+                    Continuar
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default Question;
--- quizz/src/components/Welcome.jsx
+++ quizz/src/components/Welcome.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../context/quiz";
 
 import Quiz from "../img/quiz.svg";
 
 import "./Welcome.css";
 
 const Welcome = () => {
-  return (
-    <div id="welcome">
-      <h2>Seja bem-vindo</h2>
-      <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
-      <img src={Quiz} alt="Início do Quizz" />
-    </div>
-  );
+    const [quizState, dispatch] = useContext(QuizContext);
+    return (
+        <div id="welcome">
+            <h2>Seja bem-vindo</h2>
+            <p>Clique no botão abaixo para começar:</p>
+            <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
+            <img src={Quiz} alt="Início do Quizz" />
+        </div>
+    );
 };
 
 export default Welcome;
