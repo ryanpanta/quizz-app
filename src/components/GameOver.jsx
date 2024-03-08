1721361059e032dc6e2124d1f04f9b9a4d2aa9cf
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-03-08 00:13:27 -0300

    feat: responsive layouts for small devices

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,19 +2,31 @@ import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 import WellDone from "../img/welldone.svg";
 
-import './GameOver.css'
+import "./GameOver.css";
 
 const GameOver = () => {
-
     const [quizState, dispatch] = useContext(QuizContext);
-    
+
     return (
         <div id="gameover">
             <h2>Fim de jogo!</h2>
             <p>Pontuação: {quizState.score}</p>
-            <p>Você acertou {quizState.score} de {quizState.questions.length} perguntas.</p>
+            <p>
+                Você acertou {quizState.score} de {quizState.questions.length}{" "}
+                perguntas.
+            </p>
+            {quizState.score >= 8 && (
+                <p className="feedback">Parabéns, você arrasou 💪</p>
+            )}
+            {quizState.score <= 4 && (
+                <p className="feedback">
+                    Você está indo bem, que tal tentar novamente? 🥰
+                </p>
+            )}
             <img src={WellDone} alt="Fim do Quiz" />
-            <button onClick={() => dispatch({type: "NEW_GAME"})}>Reiniciar</button>
+            <button onClick={() => dispatch({ type: "NEW_GAME" })}>
+                Reiniciar
+            </button>
         </div>
     );
 };
